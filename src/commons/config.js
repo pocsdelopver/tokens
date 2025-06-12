fe9389c207645a0cd32733147429fa27df6473e4
Zahid71187 <zahidyramirezgonzalez@gmail.com>
2025-06-12 10:07:39 -0600

    Despliegue tokens

--- src/commons/config.js
+++ src/commons/config.js
@@ -2,7 +2,7 @@ const crypto = require('crypto')
 
 const config = {
     secretKey: crypto.createHash('sha256').update('mi_clave_super_secreta').digest('hex'),
-    port: 3000
+    port: 8080
 }
 
 module.exports = config 
