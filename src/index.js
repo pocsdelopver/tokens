f8fdfe32c002c1ed7701f4e2b6fdefd5de914e60
Zahid71187 <zahidyramirezgonzalez@gmail.com>
2025-06-12 10:12:52 -0600

    Despliegue tokens

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ app.use(express.json())
 // Routes
 app.use('/', authRoutes)
 
+app.get('/', (req, res) => {
+  res.send('Bienvenido a la API de Prestamos');
+}
+)
+
 app.listen(config.port, () => {
     console.log(`Servidor autenticación escuchando en puerto ${config.port}`)
 })
